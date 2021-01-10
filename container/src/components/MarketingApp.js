a36a0b1aea97315252df4f78e80faf6a4ebb0b54
Edwin Torres <codesandtags@gmail.com>
2021-01-09 19:52:26 -0500

    featurel: Added navigation to container and marketing

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,13 +1,25 @@
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import { mount as marketingMount } from "marketing/MarketingApp";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    marketingMount(ref.current);
-  });
+    const { onParentNavigate } = marketingMount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref}></div>;
 };
--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -1,16 +1,30 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPLugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
   mode: "development",
   devServer: {
-    port: 8081,
+    port: 8083,
     historyApiFallback: {
       index: "index.html",
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   plugins: [
+    new ModuleFederationPLugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
