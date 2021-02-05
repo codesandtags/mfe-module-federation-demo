62150c5525ab35db7b41eedf30686fb1b624314d
Edwin Torres <codesandtags@gmail.com>
2021-02-04 22:55:27 -0500

    chore: Update title

--- marketing/src/components/Landing.js
+++ marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Hello Demo
             </Typography>
             <Typography
               variant="h5"
